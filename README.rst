bd4ddd568b038b6bab20db29a15fd85dfd222ea8
Nicolas Guarin-Zapata <nicoguarin@gmail.com>
2019-08-27 11:00:29 -0500

    Add Zener ratio to cubic materials

--- README.rst
+++ README.rst
@@ -1,26 +1,31 @@
-# Material database
+=================
+Material database
+=================
 
 This repository has properties for different groups of material. The main idea
 is to provide accessible properties for comparison. References for the material
-properties are provided in the file `materials_references.bib`.
+properties are provided in the file ``materials_references.bib``.
 
-## Files format
+Files format
+------------
 
-Right now the properties are stored in Tab Separated Value files (.tsv)
+Right now the properties are stored in Tab Separated Value files (``.tsv``)
 for simplicity. They are not a robust solution, but they can be easily imported
 in Python and JavaScript scripts. For material properties, minimum and
 maximum values are provided in common units.
 
 
-## Collaboration
+Collaboration
+-------------
 
 If you want to add properties for materials in some of the groups you can
 check the format and units in the corresponding file. The references should
-go in `materials_references.bib` and the bibtex-key should go in the field
+go in ``materials_references.bib`` and the bibtex-key should go in the field
 for references of the file.
 
-## License
+License
+-------
 
 The data present in this repository is made available under the
-[Open Data Commons Attribution License](http://opendatacommons.org/licenses/by/1.0).
-Code is availabe under [MIT license](https://opensource.org/licenses/mit-license.php).
+`Open Data Commons Attribution License <http://opendatacommons.org/licenses/by/1.0>`__.
+Code is availabe under `MIT license <https://opensource.org/licenses/mit-license.php>`__.
--- scripts/transverse_isotropic_anisotropy_index.py
+++ scripts/transverse_isotropic_anisotropy_index.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Comparison of anisotropy index for transverse isotropic materials
+Comparison of anisotropy indices for transverse isotropic materials
 
 @author: Nicolas Guarin-Zapata
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-fpath = "Transverse isotropic.tsv"
 
 
 def aniso(C11, C12, C13, C33, C44):
@@ -65,25 +62,27 @@ def aniso_ratio(C11, C12, C13, C33, C44):
     return aqP, aqS, aS
 
 
-cols = range(2,12)
-mats = np.loadtxt(fpath, skiprows=1, usecols=(0,), dtype=str)
-props = np.loadtxt(fpath, skiprows=1, usecols=cols, delimiter='\t')
-C11 = 0.5*(props[:, 0] + props[:, 1])
-C12 = 0.5*(props[:, 2] + props[:, 3])
-C13 = 0.5*(props[:, 4] + props[:, 5])
-C33 = 0.5*(props[:, 6] + props[:, 7])
-C44 = 0.5*(props[:, 8] + props[:, 9])
-a = aniso(C11, C12, C13, C33, C44)
-aqP, aqS, aS = aniso_ratio(C11, C12, C13, C33, C44)
-x = np.array(range(len(a)))
-pal = sns.color_palette()
-plt.bar(x, np.abs(1 - a)/np.max(np.abs(1 - a)), align='center',
-        width=0.4, color=pal[0])
-plt.bar(x + 0.4, (aqP + aqS + aS)/3/np.max((aqP + aqS + aS)/3),
-        align='center', width=0.4, color=pal[1])
-plt.xticks(x, mats, rotation='vertical')
-plt.xlim(-0.5, len(a) - 0.5)
-plt.ylabel("Anisotropy index")
-plt.tight_layout()
-plt.savefig("aniso_index.png", dpi=300)
-plt.show()
+if __name__ == "__main__":
+    plt.style.use("seaborn")
+    fpath = "../transverse_isotropic.tsv"
+    cols = range(2,12)
+    mats = np.loadtxt(fpath, skiprows=1, usecols=(0,), dtype=str)
+    props = np.loadtxt(fpath, skiprows=1, usecols=cols, delimiter='\t')
+    C11 = 0.5*(props[:, 0] + props[:, 1])
+    C12 = 0.5*(props[:, 2] + props[:, 3])
+    C13 = 0.5*(props[:, 4] + props[:, 5])
+    C33 = 0.5*(props[:, 6] + props[:, 7])
+    C44 = 0.5*(props[:, 8] + props[:, 9])
+    a = aniso(C11, C12, C13, C33, C44)
+    aqP, aqS, aS = aniso_ratio(C11, C12, C13, C33, C44)
+    x = np.array(range(len(a)))
+    plt.bar(x, np.abs(1 - a)/np.max(np.abs(1 - a)), align='center',
+            width=0.4)
+    plt.bar(x + 0.4, (aqP + aqS + aS)/3/np.max((aqP + aqS + aS)/3),
+            align='center', width=0.4)
+    plt.xticks(x, mats, rotation='vertical')
+    plt.xlim(-0.5, len(a) - 0.5)
+    plt.ylabel("Anisotropy index")
+    plt.tight_layout()
+    plt.savefig("aniso_index.png", dpi=300)
+    plt.show()
